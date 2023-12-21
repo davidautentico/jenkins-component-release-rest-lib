ef0bec0c7d4956048e3a56c18a6035f31e88edac
David <davidautentico@gmail.com>
2023-12-21 19:02:34 +0100

    Update componentReleaseRest.groovy

--- vars/componentReleaseRest.groovy
+++ vars/componentReleaseRest.groovy
@@ -1,7 +1,8 @@
 def httpGet(Map args) {
-    def response = args.jenkinsWorkflowScript.invokeMethod 'httpRequest', [[(args.url): url]] as Object[]
-    if (response.status != 200) {
-        jenkinsWorkflowScript.invokeMethod 'echo', [response.content] as Object[]
-    }
+    println args
+    //def response = args.jenkinsWorkflowScript.invokeMethod 'httpRequest', [[(args.url): url]] as Object[]
+    //if (response.status != 200) {
+    //    jenkinsWorkflowScript.invokeMethod 'echo', [response.content] as Object[]
+    //}
     response.content
 }
